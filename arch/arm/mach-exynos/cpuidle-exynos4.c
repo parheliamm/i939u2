fddbe3c844a5919a723eb094567cdca511c05426
Chenxi Mao <chenxi.mao2013@gmail.comcd>
2013-03-20 17:04:00 +0800

    cpuidle:enable aftr

--- arch/arm/mach-exynos/cpuidle-exynos4.c
+++ arch/arm/mach-exynos/cpuidle-exynos4.c
@@ -90,7 +90,7 @@ unsigned int log_en;
 module_param_named(log_en, log_en, uint, 0644);
 
 #if defined(CONFIG_MACH_MIDAS) || defined(CONFIG_SLP)
-#define CPUDILE_ENABLE_MASK (ENABLE_LPA)
+#define CPUDILE_ENABLE_MASK (ENABLE_AFTR | ENABLE_LPA)
 #else
 #define CPUDILE_ENABLE_MASK (ENABLE_AFTR | ENABLE_LPA)
 #endif
